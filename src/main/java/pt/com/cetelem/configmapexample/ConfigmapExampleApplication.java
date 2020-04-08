72dd4bbdd9d79e2de3ad422210d0991a25648d97
pbayona <eddybayonne@gmail.com>
2020-04-08 16:35:57 +0100

    configMap example with literal creations

---
+++ src/main/java/pt/com/cetelem/configmapexample/ConfigmapExampleApplication.java
@@ -0,0 +1,13 @@
+package pt.com.cetelem.configmapexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigmapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigmapExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pt/com/cetelem/configmapexample/controller/ConfigMapController.java
@@ -0,0 +1,34 @@
+package pt.com.cetelem.configmapexample.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/configmap")
+public class ConfigMapController {
+
+    @Value("${default.configmap}")
+    private String defaultConfigMapMsg;
+
+    @GetMapping
+    public ResponseEntity<String> getConfigMap(@RequestBody final String configMap) {
+        if (configMap == null || configMap.isEmpty()) {
+            return new ResponseEntity<>("Given configMap is blank", HttpStatus.BAD_REQUEST);
+        }
+        System.getenv().forEach((s, s2) -> System.out.println(s+" : "+s2));
+
+        String foundConfigMap = System.getenv(configMap);
+        if (foundConfigMap == null){
+            return new ResponseEntity<>("ConfigMap not found. Please provide it using K8S configMap", HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(String.format(defaultConfigMapMsg, foundConfigMap));
+
+    }
+}
